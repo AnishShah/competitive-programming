47df80c4dccb8570dc2a4dc4f9e77d8a37045457
Anish Shah <shah.anish07@gmail.com>
2014-07-18 19:23:05 +0530

    comments in PE/Problem1.cpp

--- Project Euler/Problem1.cpp
+++ Project Euler/Problem1.cpp
@@ -18,28 +18,13 @@
 #include <cstring>
 #include <ctime>
 using namespace std;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-#define sz(c) (int)(c).size()
-#define pb push_back
-#define all(c) (c).begin(), (c).end()
-#define tr(c, i) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
-#define present(c, x) ((c).find(x) != (c).end())
-#define cpresent(c, x) (find(all(c), x) != (c).end())
-#define REP(i, n) for(int i = 0; i < (n); i++)
-#define REPD(i, n) for(int i = (n)-1; i >= 0; i--)
-#define FOR(i, a, b) for(int i = (a); i <= (b); i++)
-#define FORD(i, a, b) for(int i = (a); i >= (b); i--)
-#define mp make_pair
-#define CHECK(S, j) (S & (1 << j))
-#define SET(S, j) (S |= (1 << 0000j))
-#define SETALL(S, j) (S = (1 << j)-1)
-#define UNSET(S, j) (S &= ~(1 << j))
-#define TOGGLE(S, j) (S ^= (1 << j))
 #define LL long long
-#define PI acos(-1)
-template<class T>string tostring(T t){stringstream s;s<<t;return s.str();}
-
+/* Project Euler - #1 - Multiples of 3 and 5
+ * https://projecteuler.net/problem=1
+ *
+ * Use Arithmetic progression to find the answer in O(1) 
+ * constant time.
+ */
 LL sumDivisibleBy(int divisible, LL N){
     return divisible*(N/divisible)*((N/divisible) + 1)/2;
 }
