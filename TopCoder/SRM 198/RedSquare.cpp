fd0658fa89d75a990be3519efb344923689512dc
Anish Shah <shah.anish07@gmail.com>
2014-07-20 02:26:25 +0530

    Simpler method for SRM 198 500

--- TopCoder/SRM 198/RedSquare.cpp
+++ TopCoder/SRM 198/RedSquare.cpp
@@ -21,32 +21,19 @@
 using namespace std;
 #define sz(c) (int)(c).size()
 #define REP(i, n) for(int i = 0; i < (n); i++)
-#define FOR(i, a, b) for(int i = (a); i <= (b); i++)
-template<class T>string tostring(T t){stringstream s;s<<t;return s.str();}
 
-
+/*
+ * SRM 198 DIV 2 500 Problem
+ */
 class RedSquare {
 public:
 	int countTheEmptyReds(int maxRank, int maxFile, vector <int> rank, vector <int> file) {
-		int checkersboard[51][51];
-		FOR(i, 1, maxRank){
-			FOR(j, 1, maxFile){
-				if(maxFile % 2 == 1){
-					checkersboard[i][j] = ((i % 2 == 1) & (j % 2 == 0)) | ((i % 2 == 0) & (j % 2 == 1));
-				}else{
-					checkersboard[i][j] = ((i % 2 == 1) & (j % 2 == 1)) | ((i % 2 == 0) & (j % 2 == 0));
-				} 
-			}
-		}
-		REP(i, sz(rank)){
-			checkersboard[rank[i]][file[i]] = 0;
-		}
 		int ans = 0;
-		FOR(i, 1, maxRank){
-			FOR(j, 1, maxFile){
-				ans += checkersboard[i][j];
+		REP(k, sz(rank)){
+			if((rank[k]+file[k])%2 == maxFile%2){
+				ans++;
 			}
 		}
-		return ans;
+		return (maxRank*maxFile)/2 - ans;
 	}
 };
