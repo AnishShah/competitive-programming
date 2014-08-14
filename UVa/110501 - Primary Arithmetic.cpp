7ea720bc48e9e856abd6379eb959d6808f8d81c0
Anish Shah <shah.anish07@gmail.com>
2014-08-14 21:01:57 +0530

    UVa

---
+++ UVa/110501 - Primary Arithmetic.cpp
@@ -0,0 +1,67 @@
+#include <vector>
+#include <queue>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+using namespace std;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+#define sz(c) (c).size()
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define tr(c, i) for(__typeof((c).begin()) i = (c).begin(); i != (c).end(); i++)
+#define present(c, x) ((c).find(x) != (c).end())
+#define cpresent(c, x) (find(all(c), x) != (c).end())
+#define REP(i, n) for(int i = 0; i < (n); i++)
+#define REPD(i, n) for(int i = (n)-1; i >= 0; i--)
+#define FOR(i, a, b) for(int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for(int i = (a); i >= (b); i--)
+#define mp make_pair
+#define LL long long
+#define CHECK(S, j) (S & (1 << j))
+#define SET(S, j) (S |= (1 << j))
+#define SETALL(S, j) (S = (1 << j)-1)
+#define UNSET(S, j) (S &= ~(1 << j))
+#define TOGGLE(S, j) (S ^= (1 << j))
+#define modulo 1000000007
+template<class T>string tostring(T t){stringstream s;s<<t;return s.str();}
+
+int main(){
+    int x, y;
+    cin >> x >> y;
+    while(x || y){
+        int cnt = 0;
+        int carry = 0;
+        while(x || y){
+            int u1 = x % 10, u2 = y % 10;
+            x/=10, y/=10;
+            cnt += (u1 + u2 + carry)/10;
+            carry = (u1 + u2 + carry)/10;
+        }
+        if(cnt){
+            if(cnt == 1){
+                cout << "1 carry operation." << endl;
+            }else{
+                cout << cnt << " carry operations." << endl;
+            }
+        }else{
+            cout << "No carry operation." << endl;
+        }
+        cin >> x >> y;
+    }
+}
